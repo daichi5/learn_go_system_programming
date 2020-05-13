87ce3da3b5e8655d1b4d872d17321aa2dd292585
sawa <sw41dc@gmail.com>
2020-05-14 00:54:04 +0900

    sync wait group

---
+++ chapter13/3.wait_group/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		fmt.Println("work1")
+		wg.Done()
+	}()
+	go func() {
+		fmt.Println("work2")
+		wg.Done()
+	}()
+	wg.Wait()
+	fmt.Println("finished")
+}
