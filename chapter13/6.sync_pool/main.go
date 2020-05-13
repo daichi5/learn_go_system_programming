8b35bb0b760b6c048899b13464296260615e0412
sawa <sw41dc@gmail.com>
2020-05-14 00:54:39 +0900

    sync pool

---
+++ chapter13/6.sync_pool/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	var count int
+	pool := sync.Pool{
+		New: func() interface{} {
+			count++
+			return fmt.Sprintf("created: %d", count)
+		},
+	}
+
+	pool.Put("manualy added: 1")
+	pool.Put("manualy added: 2")
+	fmt.Println(pool.Get())
+	fmt.Println(pool.Get())
+	fmt.Println(pool.Get())
+	pool.Put("manualy added: 3")
+	runtime.GC()
+	fmt.Println(pool.Get())
+
+}
